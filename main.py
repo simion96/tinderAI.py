557cfc12131132938fb7e0f0a3d1e57e47389ef6
Simion Putina <simionp96@gmail.com>
2016-07-06 21:27:35 +0100

    can get reccomandations and print to file

--- main.py
+++ main.py
@@ -24,11 +24,7 @@ url = 'https://api.gotinder.com/auth'
 headers ={'Content-Type': 'application/json', 'User-Agent':'Tinder/4.8.2 (iPhone; iOS 9.1; Scale/2.00)'}
 payload = {'force_refresh' : 'False', 'facebook_id' : fbID, 'facebook_token' : fbToken}
 r = requests.post(url, headers=headers, data = json.dumps(payload))
-print headers
-print r.url
-print r.headers
-print r.request
-print r.status_code
+
 
 print 'token is: ' + str(fbToken)
 print 'fbid is: ' + str(fbID)
@@ -37,7 +33,27 @@ rjson =  json.loads(r.text)
 print "token is: " + rjson['token']
 tinder_token = rjson['token']
 
+tinder_headers = {'X-Auth-Token': tinder_token,
+                  'Authorization': 'Token token="{0}"'.format(tinder_token).encode('ascii', 'ignore')
+                  }
+print tinder_headers
 
+#getting reccomandations
+url2 = 'https://api.gotinder.com/user/recs'
+
+tinder_headers2 = {'X-Auth-Token': tinder_token,
+                  'Authorization': 'Token token="{0}"'.format(tinder_token).encode('ascii', 'ignore'),
+                   'locale': 'en-GB'
+                  }
+r = requests.post(url2, headers = tinder_headers2)
+recs_json = r.text
+with open('data.txt', 'w') as outfile:
+    json.dump(r.text, outfile, sort_keys=True, indent=4)
+print r.url
+print r.headers
+print r.request
+print r.status_code
+print r.text
 
 
 
