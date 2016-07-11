3dddd78771699e9ba54f3f33b3baa311c4d8275f
Simion Putina <simionp96@gmail.com>
2016-07-11 18:22:30 +0100

    can now like into oblivion

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import time
 import urllib2
 from pprint import pprint
 import requests
+import datetime
 
 fbToken = os.getenv('FB_TOKEN')
 fbID = os.getenv('FB_ID')
@@ -51,65 +52,96 @@ tinder_headers = {'X-Auth-Token': tinder_token,
 print tinder_headers
 
 #getting reccomandations
-url2 = 'https://api.gotinder.com/user/recs'
+def get_recs():
+    url2 = 'https://api.gotinder.com/user/recs'
 
-tinder_headers2 = {'X-Auth-Token': tinder_token,
-                  'Authorization': 'Token token="{0}"'.format(tinder_token).encode('ascii', 'ignore'),
-                   'locale': 'en-GB'
-                  }
-r = requests.post(url2, headers = tinder_headers2)
-with open('data.txt', 'w') as outfile:
-    json.dump(r.text, outfile, sort_keys=True, indent=4)
-with open('data.txt') as data_file:
-    recs_json = json.load(data_file)
-#pprint(recs_json)
-recs_json2 = byteify(recs_json)
-print r.url
-print r.headers
-print r.request
-print r.status_code
-print json.dumps(json.loads(r.text), indent =4)
+    tinder_headers2 = {'X-Auth-Token': tinder_token,
+                      'Authorization': 'Token token="{0}"'.format(tinder_token).encode('ascii', 'ignore'),
+                       'locale': 'en-GB'
+                      }
+    r = requests.post(url2, headers = tinder_headers2)
+    with open('data.txt', 'w') as outfile:
+        json.dump(r.text, outfile, sort_keys=True, indent=4)
+    with open('data.txt') as data_file:
+        recs_json = json.load(data_file)
+    #pprint(recs_json)
+    recs_json2 = byteify(recs_json)
+    print r.url
+    print r.headers
+    print r.request
+    print r.status_code
+    #print json.dumps(json.loads(r.text), indent =4) - prints the request
 
-dict = json.loads(recs_json2)
+    dict = json.loads(recs_json2)
+    return dict
 
-print type(dict)
+#print type(dict)
 print "------------LADIES--------------"
 
 def like_recs():
-    liked = ""
-    with open("liked", "r") as text_file:
-        liked = text_file.read()
-    instagrams = ""
-    with open("instagrams", "r") as text_file:
-        instagrams = text_file.read()
-    for i in dict['results']:
-        time.sleep(1)
-        link = 'https://api.gotinder.com/like/{0}'.format(i["_id"])
-        liking_header = {'X-Auth-Token': tinder_token,
-                         'Authorization': 'Token token="{0}"'.format(tinder_token).encode('ascii', 'ignore'),
-                         'firstPhotoID': ''+str(i['photos'][0]['id'])
-                         }
-        likereq = requests.get(link, headers = liking_header)
-        #print i['name'] + ' - ' +  i['_id']
-        print 'status: ' + str(likereq.status_code) + ' text: ' + str(likereq.text)
-        liked += str(i['name']) + ' - ' + str(i['_id']) + ' - ' + str(i['photos'][0]['url']) + '\n'
-        try:
-            if 'instagram' in i:
-              instagrams+= str(i['instagram']['username'] + " ")
-            else:
-                print "nnonono"
-        except KeyError as ex:
-            print 'nah mate'
-        #print "photoid " + str(i['photos'][0]['id'])
-    with open("liked", "w") as text_file:
-        text_file.write(liked)
-    with open("instagrams", "w") as text_file:
-        text_file.write(instagrams)
+    counter = 0
+    try:
+        while counter < 3:
+            resultsr = get_recs()
+            results = resultsr['results']
+            liked = ""
+            with open("liked", "r") as text_file:
+                liked = text_file.read()
+            instagrams = ""
+            with open("instagrams", "r") as text_file:
+                instagrams = text_file.read()
+            for i in results:
+                time.sleep(1)
+                link = 'https://api.gotinder.com/like/{0}'.format(i["_id"])
+                liking_header = {'X-Auth-Token': tinder_token,
+                                 'Authorization': 'Token token="{0}"'.format(tinder_token).encode('ascii', 'ignore'),
+                                 'firstPhotoID': ''+str(i['photos'][0]['id'])
+                                 }
+                likereq = requests.get(link, headers = liking_header)
+                #print i['name'] + ' - ' +  i['_id']
+                print 'status: ' + str(likereq.status_code) + ' text: ' + str(likereq.text)
+                liked += str(i['name']) + ' - ' + str(i['_id']) + ' - ' + str(i['photos'][0]['url']) + '\n'
+                try:
+                    if 'instagram' in i:
+                      instagrams+= str(i['instagram']['username'] + " ")
+                    else:
+                        print "nnonono"
+                except KeyError as ex:
+                    print 'nah mate'
+                #print "photoid " + str(i['photos'][0]['id'])
+            with open("liked", "w") as text_file:
+                text_file.write(liked)
+            with open("instagrams", "w") as text_file:
+                text_file.write(instagrams)
+            #resultsraw = get_recs()
+            #results = resultsraw['results']
+            counter += 1
+
+
+    except Exception as ex:
+        print "hit an exception i guess"
+        print ex
+
+#err this actually gets matches, but could still be useful at some point
+def get_updates():
+    timenow = datetime.datetime.now().isoformat()
+    print timenow
+    tinder_headers3 = {'X-Auth-Token': tinder_token,
+                       'Authorization': 'Token token="{0}"'.format(tinder_token).encode('ascii', 'ignore'),
+                       'last_activity_date': '{}'.format(timenow)
+                       }
+    url3 = 'https://api.gotinder.com/updates'
+    r3 = requests.post(url3, headers = tinder_headers3)
+    print json.dumps(json.loads(r3.text), indent=4)
+    return r3.text
+
 
 like_recs()
 
 #print type(recs_json2)
 #pprint(recs_json2)
+
+
 
 like_headers2 = {'X-Auth-Token': tinder_token,
                    'Authorization': 'Token token="{0}"'.format(tinder_token).encode('ascii', 'ignore'),
